3bf80f0e5e26fcf21e6cf84a412a22769dfec04e
Anhelina <anhelina.kashuba.22@pnu.edu.ua>
2024-10-29 18:28:32 +0200

    Add image and update resume page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'Hello, world!'
+def home():
+    return 'Welcome to the Flask App!'
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
