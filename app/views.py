2facf72f8c5f5e8192b473e710cfa6cf1ca26668
Anhelina <anhelina.kashuba.22@pnu.edu.ua>
2024-11-18 23:07:32 +0200

    Initial commit for dev3

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ def home():
 
 #users
 
-@app.route("/hi/<string:name>")   #/hi/ivan?age=45
+@app.route("/hi/<string:name>")  
 def greetings(name):
     name = name.upper()
     age = request.args.get("age", None, int)   
@@ -24,6 +24,6 @@ def greetings(name):
 
 @app.route("/admin")
 def admin():
-    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
+    to_url = url_for("greetings", name="Anhelina", age=19, _external=True)   
     print(to_url)
     return redirect(to_url)
